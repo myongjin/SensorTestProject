aec1a79fa0f488e003b2d696abf23f19cf3c05ab
myongjin <49631590+myongjin@users.noreply.github.com>
2020-12-07 12:09:47 +0000

    Pressure DLL added

---
+++ Assets/Scripts/Sensors/PressureMeasure.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PressureMeasure : MonoBehaviour
+{
+    [DllImport("PressurePad")]
+    public static extern bool InitDevice();
+
+    [DllImport("PressurePad")]
+    public static extern bool SetSensitivity(int value);
+
+    [DllImport("PressurePad")]
+    public static extern float[] GetPressureArray();
+
+    [DllImport("PressurePad")]
+    public static extern void CloseDevice();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (InitDevice())
+        {
+            Debug.Log("Device was initialised successfully");
+        }
+        else
+        {
+            Debug.Log("Failed to initialise the device");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
